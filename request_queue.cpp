e3bc877ff1e42653c1e68d0cbc00dcbfa1f7b64d
vladislav-markov <mail.vlad.ma@yandex.ru>
2021-04-27 10:32:44 +0300

    minor change

--- request_queue.cpp
+++ request_queue.cpp
@@ -18,3 +18,40 @@ RequestQueue::GetNoResultRequests() const
         return number_of_no_result_requests;
     }
 
+void
+RequestQueue::UpdateRequestsResultInfo(
+        const std::vector< Document > & found_documents )
+    {
+        if( requests_.size() == sec_in_day_ )
+            {
+                requests_.pop_front();
+            }
+
+        requests_.push_back( { !found_documents.empty() } );
+    }
+
+std::vector< Document >
+RequestQueue::AddFindRequest(
+        const std::string & raw_query,
+        const DocumentStatus status )
+    {
+        const std::vector< Document > found_documents
+                        = server_.FindTopDocuments( raw_query, status );
+
+        UpdateRequestsResultInfo( found_documents );
+
+        return found_documents;
+    }
+
+std::vector< Document >
+RequestQueue::AddFindRequest(
+        const std::string & raw_query )
+    {
+        const std::vector< Document > found_documents
+                        = server_.FindTopDocuments( raw_query );
+
+        UpdateRequestsResultInfo( found_documents );
+
+        return found_documents;
+    }
+
--- request_queue.h
+++ request_queue.h
@@ -10,53 +10,34 @@ class RequestQueue
 
             explicit RequestQueue( const SearchServer & search_server );
 
-            template < typename SearchParameter >
+            template < typename DocumentPredicate >
             std::vector< Document >
             AddFindRequest(
                     const std::string & raw_query,
-                    SearchParameter search_parameter )
+                    const DocumentPredicate document_predicate )
                 {
-                    return AddFindRequest( raw_query, std::optional< SearchParameter >( search_parameter ) );
+                    const std::vector< Document > found_documents
+                                    = server_.FindTopDocuments( raw_query, document_predicate );
+
+                    UpdateRequestsResultInfo( found_documents );
+
+                    return found_documents;
                 }
 
             std::vector< Document >
             AddFindRequest(
-                    const std::string & raw_query )
-                {
-                    return AddFindRequest( raw_query, []() -> std::optional< DocumentStatus > { return std::nullopt; }() );
-                }
+                    const std::string & raw_query,
+                    const DocumentStatus status );
+
+            std::vector< Document >
+            AddFindRequest(
+                    const std::string & raw_query );
 
             int
             GetNoResultRequests() const;
 
+
         private:
-
-            template < typename SearchParameter >
-            std::vector< Document >
-            AddFindRequest(
-                    const std::string & raw_query,
-                    std::optional< SearchParameter > search_parameter = std::nullopt )
-                {
-                    if( requests_.size() == sec_in_day_ )
-                        {
-                            requests_.pop_front();
-                        }
-
-                    std::vector< Document > found_docs;
-
-                    if( search_parameter )
-                        {
-                            found_docs = server_.FindTopDocuments( raw_query, *search_parameter );
-                        }
-                    else
-                        {
-                            found_docs = server_.FindTopDocuments( raw_query );
-                        }
-
-                    requests_.push_back( { !found_docs.empty() } );
-
-                    return found_docs;
-                }
 
             struct QueryResult
                 {
@@ -68,6 +49,10 @@ class RequestQueue
             static constexpr int sec_in_day_ = 1440;
 
             const SearchServer & server_;
+
+            void
+            UpdateRequestsResultInfo(
+                    const std::vector< Document > & found_documents );
     };
 
 
